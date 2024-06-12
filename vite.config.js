31fe64ea1e4f14a66c474bba3d860884b328429a
Mahima Devi <72203327+mahimadevi@users.noreply.github.com>
2024-06-12 17:29:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@material-ui/core'],
+      external: /^@material-ui\/core(\/.*)?$/,
     },
   },
 });
